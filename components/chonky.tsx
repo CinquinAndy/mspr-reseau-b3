437e663e3262e0f152f8ffe8a037f26cd9bdb4ad
CinquinAndy <cinquin.andy@gmail.com>
2022-02-27 23:33:20 +0100

    files

--- components/chonky.tsx
+++ components/chonky.tsx
@@ -4,43 +4,15 @@ export const MyFileBrowser = () => {
         const files = [
             {
                 id: '1',
-                name: 'fiche_1.pdf',
+                name: 'andyc_med.txt',
             },
             {
                 id: '2',
-                name: 'fiche_2.pdf',
+                name: 'brevallf_med.txt',
             },
             {
                 id: '3',
-                name: 'fiche_3.pdf',
-            },
-            {
-                id: '4',
-                name: 'fiche_4.pdf',
-            },
-            {
-                id: '5',
-                name: 'fiche_5.pdf',
-            },
-            {
-                id: '6',
-                name: 'fiche_6.pdf',
-            },
-            {
-                id: '7',
-                name: 'fiche_7.pdf',
-            },
-            {
-                id: '8',
-                name: 'fiche_8.pdf',
-            },
-            {
-                id: '9',
-                name: 'fiche_9.pdf',
-            },
-            {
-                id: 'mcd',
-                name: '_tuto.pdf',
+                name: 'mathisg_med.txt',
             },
         ];
         const folderChain = [
